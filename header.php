165fb19478b7dfd529b0f1ea9926081354ce2b68
Vladimir Gorbanev <git-commit-address>
2021-11-13 20:04:11 +0700

    start

---
+++ header.php
@@ -0,0 +1,44 @@
+<div class="header">
+    <div class="header__top">
+        <div class="header__title">
+            <span>Magic Chest</span>
+        </div>
+        <div class="header__research">
+            <input type="text" name="" id="research__main">
+            <input type="button" value="" class="research__main__button">
+        </div>
+        <div class="header__button">
+            <div class="header__orders">
+                <div class="icon">
+
+                </div>
+                <div class="text__icon">
+                    Заказы
+                </div>
+            </div>
+            <div class="header__singIN">
+                <div class="icon">
+
+                </div>
+                <div class="text__icon">
+                    Войти
+                </div>    
+            </div>
+            <div class="header__basket">
+            <div class="icon">
+
+            </div>
+            <div class="text__icon">
+                Корзина
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="header__bottom">
+        <a href="">Главная</a>
+        <a href="">Католог</a>
+        <a href="">Акции</a>
+        <a href="">Оплата и доставка</a>
+        <a href="">О нас</a>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/index.css">
+
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/header_popup.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/quick_links.css">
+
+    <title>Magic-Chest</title>
+</head>
+
+<body>
+
+
+<div class="main__container">
+    <?php require "header.php " ?>
+    <div class="container">
+        <div class="main__content">
+            <div class="content__card">
+                <div class="icon__magic__chest"></div>
+            </div>
+            <div class="content__card">
+
+            <div class="quick-links-content">
+                <div class="flip-container l-card" >
+                    <div class="flipper">
+                        <div class="front">
+                            <h3>Альбомы</h3>
+                            <p>сборники игр на липучках для деток всех возрастов, развивающие самые разные уменая и навыки</p>
+                        </div>
+
+                        <div class="back">
+                            <h3>Альбомы</h3>
+                            <p>сборники игр на липучках для деток всех возрастов, развивающие самые разные уменая и навыки</p>
+                            <input type="button" value="Заказать">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flip-container c-card" >
+                    <div class="flipper">
+                        <div class="front">
+                            <h3>Прописи</h3>
+                            <p>подготовка руки к письму от первых шагов до подготовки к школе</p>
+                        </div>
+
+                        <div class="back">
+                            <h3>Прописи</h3>
+                            <p>подготовка руки к письму от первых шагов до подготовки к школе</p>
+                            <input type="button" value="Заказать">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flip-container r-card" >
+                    <div class="flipper">
+                        <div class="front">
+                            <h3>Карточки</h3>
+                            <p>подготовка руки к письму от первых шагов до подготовки к школе</p>
+                        </div>
+
+                        <div class="back">
+                            <h3>Карточки</h3>
+                            <p>подготовка руки к письму от первых шагов до подготовки к школе</p>
+                            <input type="button" value="Заказать">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+            <div class="content__card">
+                
+            </div>
+        </div>
+    </div>
+    <?php require "footer.php " ?>
+</div>
+</body>
+
+</html>
