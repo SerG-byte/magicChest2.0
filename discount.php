dc59d74fac1173a724b54650e70a37eb9143f2ca
Vladimir Gorbanev <git-commit-address>
2021-12-05 17:50:25 +0700

    new pages

---
+++ discount.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styleSite.css">
+
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/header_popup.css">
+    <link rel="stylesheet" href="css/footer.css">
+    
+
+
+    <title>Magic-Chest</title>
+</head>
+
+<body>
+
+
+<div class="main__container">
+    <?php require "header.php " ?>
+    <div class="container">
+        <div class="main__content">
+            Страница в разработке! :c
+        </div>
+    </div>
+    <?php require "footer.php " ?>
+</div>
+</body>
+</html>
